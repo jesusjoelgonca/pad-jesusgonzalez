dfab2a007c1a4dcd6769b6cb8ca51eae9f42c9a9
Jesus Gonzalez <107076643+jesusjoelgonca@users.noreply.github.com>
2025-05-22 18:42:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup, find_packages
 
+#setup(
+ #   name="edu_pad",
+  #  version="0.0.1",
+   # author="Jesus Gonzaez",
+    #author_email="",
+    #description="ETL para análisis de datos del dólar",
+    #py_modules=["actividad1", "actividad2"],
+    #install_requires=[
+     #   "pandas",
+      #  "openpyxl",
+       # "requests",
+        #"beautifulsoup4"
+    #]
+#)
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="edu_pad",
     version="0.0.1",
-    author="Jesus Gonzaez",
+    author="Jesus Gonzalez",
     author_email="",
     description="ETL para análisis de datos del dólar",
     py_modules=["actividad1", "actividad2"],
-    install_requires=[
-        "pandas",
-        "openpyxl",
-        "requests",
-        "beautifulsoup4"
-    ]
+    install_requires=requirements,
 )
