d7dc7e45d9c966f9d359bcaf9d1f78b2e04230a6
Jesus Gonzalez <107076643+jesusjoelgonca@users.noreply.github.com>
2025-06-07 20:06:29 +0000

    Crear directorio para guardar el archivo CSV antes de la exportación

--- actividades/actividad1/src/main_extracto.py
+++ actividades/actividad1/src/main_extracto.py
@@ -5,6 +5,7 @@ def main():
     analizador = AnalizadorMercadoLibre(URL_MERCADOLIBRE)
     analizador.ejecutar_analisis_completo()  # Ejecuta el scraping y combina resultados
     df = analizador.obtener_dataframe()      # Obtiene el DataFrame con los datos
+    os.makedirs("static/csv", exist_ok=True)
     df.to_csv("static/csv/data_extractor.csv", index=False)  # Guarda el CSV
 
 if __name__ == "__main__":
